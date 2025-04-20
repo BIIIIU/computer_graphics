821d76a29ea86b6599478208b7a81577af184e0a
ZhangYuqi <21307130468@m.fudan.edu.cn>
2025-04-20 18:30:55 +0800

    finish shade(), getIllumination, traceRay

--- src/Light.cpp
+++ src/Light.cpp
@@ -13,12 +13,16 @@
         distToLight = std::numeric_limits<float>::max();
         // END STARTER
     }
-    void PointLight::getIllumination(const Vector3f &p, 
-                                 Vector3f &tolight, 
-                                 Vector3f &intensity, 
-                                 float &distToLight) const
+    void PointLight::getIllumination(const Vector3f &p, //点
+                                 Vector3f &tolight, //指向光源的矢量
+                                 Vector3f &intensity, //照明强度
+                                 float &distToLight) const //场景点到光源的距离
     {
         // TODO Implement point light source
         // tolight, intensity, distToLight are outputs
+        tolight = _position - p;
+        distToLight = tolight.abs();
+        tolight = tolight.normalized();
+        intensity = _color / (_falloff * distToLight * distToLight);
     }
 
