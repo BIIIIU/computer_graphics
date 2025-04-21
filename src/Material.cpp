d9dab57e279edb8d4cae9f1916adacd75e31e598
ZhangYuqi <21307130468@m.fudan.edu.cn>
2025-04-21 20:48:06 +0800

    fix specular

--- src/Material.cpp
+++ src/Material.cpp
@@ -1,4 +1,6 @@
 #include "Material.h"
+#include <iostream> 
+using namespace std;
 Vector3f Material::shade(const Ray &ray,//相机光线
     const Hit &hit,//命中点
     const Vector3f &dirToLight,
@@ -12,12 +14,12 @@ Vector3f Material::shade(const Ray &ray,//相机光线
     Vector3f diffuse = _diffuseColor * lightIntensity * clamped_dot; //漫反射
 
     //specular
-    Vector3f V = ray.getDirection().normalized(); //相机方向
+    Vector3f V = -ray.getDirection().normalized(); //相机方向,与光线方向相反
     Vector3f R = (2 * Vector3f::dot(L, N) * N - L).normalized(); //反射方向,R = 2(N·L)N - L
     float specular_dot = std::max(0.0f, Vector3f::dot(V, R)); //反射光线与相机光线的夹角
     float tmp = pow(specular_dot, _shininess); 
     Vector3f specular = _specularColor * lightIntensity * tmp; //镜面反射
-    
+
     return diffuse + specular; //返回最终颜色
 
 }
